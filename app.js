5888954fb7689818b733dfe7a75bd80a123f9c8f
Chioma Onukogu <conukogu1@gmail.com>
2022-04-13 15:24:39 -0400

    form GET

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 
+
+
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+const db_config = {
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b9516b8ecb5df4",
+  password: "98b86dc7",
+  database: "heroku_63d645c21d33683",
+};
+
+const db = mysql.createPool({connectionLimit: 5, ...db_config}); 
+
+
+app.get("/form", (req, res) => {
+  const insertQ = "SELECT * FROM heroku_63d645c21d33683.dummydata;";
+  db.query(insertQ, (err, result) => {
+    res.send(result);
+  });
+});
+
+
+
+let server = app.listen();
+
+server.on("clientError", (err, socket) => {
+  console.error(err);
+  socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
+});
+
+
+
+
 if (process.env.NODE_ENV === "production"){
     app.use(express.static('build'));
     app.get('*', (req, res) => {
