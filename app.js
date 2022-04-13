48455cfe4d35c49c24503294cb15536fab9ddc6f
Chioma Onukogu <conukogu1@gmail.com>
2022-04-13 17:17:52 -0400

    GET was modified, hopefully will work

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
 
 const path = require('path');
 
 const port = process.env.PORT || 3000;
 
+mysql://bc778fd640b8ba:78a7b63d@us-cdbr-east-05.cleardb.net/heroku_f62ce51aa2ee177?
+
+const db_config = {
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "bc778fd640b8ba",
+  password: "78a7b63d",
+  database: "heroku_f62ce51aa2ee177",
+};
+
+const db=mysql.createPool({ connectionLimit: 5, ...db_config})
+
 
 if (process.env.NODE_ENV === "production"){
     app.use(express.static('build'));
+
     app.get('*', (req, res) => {
         req.sendFile(path.resolve)(__dirname, 'build', 'index.html')
+    })
+
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get("/form", (req, res) => {
+  const insertQ = "SELECT * FROM heroku_f62ce51aa2ee177.dummydata;";
+  db.query(insertQ, (err, result) => {
+    res.send(result)
+  });
+
     })
 }
 
