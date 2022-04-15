ef7783867be8805bb6b3f4712f054a9f6cdc6e68
Chioma Onukogu <conukogu1@gmail.com>
2022-04-15 10:45:43 -0400

    changed a get route

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ const db=mysql.createPool({ connectionLimit: 5, ...db_config})
 // if (process.env.NODE_ENV === "production") {
 
 
-  app.use(express.static("build"));
+//   app.use(express.static("build"));
 
-  app.get("", (req, res) => {
-    req.sendFile(path.resolve)("build", "index.html");
-  });
+//   app.get("", (req, res) => {
+//     req.sendFile(path.resolve)("build", "index.html");
+//   });
 
      app.use(bodyParser.urlencoded({extended: true}));
 
