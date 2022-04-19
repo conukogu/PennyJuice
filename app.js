95d31e9ce1c41974621ddd148ac3807b17024654
Chioma Onukogu <conukogu1@gmail.com>
2022-04-19 15:33:40 -0400

    reversed form data

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 
   app.get("", (req, res) => {
-    req.sendFile(path.resolve)(__dirname + "build", "index.html");
+    req.sendFile(path.resolve)(__dirname, "build", "index.html");
   });
 
     //  app.use(bodyParser.urlencoded({extended: true}));
